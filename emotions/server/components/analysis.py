9527f4462343668b2151eae5a1768bd9f2e23ff9
Andrew Lee <ajyl@umich.edu>
2023-01-13 14:38:32 -0500

    loading

--- emotions/server/components/analysis.py
+++ emotions/server/components/analysis.py
@@ -13,154 +13,204 @@ def get_emotion_mms():
     ]
 
 
-utterance_component = dbc.Card(
-    [
-        dcc.Store(id="annotated-utterance-storage"),
-        dbc.CardHeader(
-            dbc.Tabs(
-                [
-                    dbc.Tab(label="Emotions", tab_id="utterance-tab-1"),
-                    dbc.Tab(label="MITI Codes", tab_id="utterance-tab-2"),
-                    dbc.Tab(label="Empathy", tab_id="utterance-tab-3"),
-                ],
-                id="utterance-tabs",
-                active_tab="utterance-tab-1",
-            )
-        ),
-        dbc.CardBody(
-            [html.H5(id="speaker"), html.Br(), html.Div(id="utterance")],
-            id="utterance-card",
-        ),
-    ]
-)
-
-micromodel_component = dbc.Card(
-    [
-        dcc.Graph(id="micromodel-results", style={"display": "none"}),
-        dcc.Store(id="emotion-classification-storage"),
-    ]
-)
-
-emotion_analysis = dbc.Card(
-    dbc.CardBody(
-        [
-            html.H4("Emotion Analysis"),
-            html.Table(
-                [
-                    html.Thead(
-                        html.Tr(
-                            [html.Th("Emotion"), html.Th("Confidence Score")]
-                        ),
-                        style={"hidden": True},
-                    ),
-                    html.Tbody(
+utterance_component = dcc.Loading(
+    id="loading-utterance",
+    children=[
+        dbc.Card(
+            [
+                dcc.Store(id="annotated-utterance-storage"),
+                dbc.CardHeader(
+                    dbc.Tabs(
                         [
-                            html.Tr(
-                                [
-                                    html.Td(id="emotion_1"),
-                                    html.Td(id="emotion_score_1"),
-                                ]
+                            dbc.Tab(
+                                label="Emotions", tab_id="utterance-tab-1"
                             ),
-                            html.Tr(
-                                [
-                                    html.Td(id="emotion_2"),
-                                    html.Td(id="emotion_score_2"),
-                                ]
+                            dbc.Tab(
+                                label="MITI Codes", tab_id="utterance-tab-2"
                             ),
-                            html.Tr(
-                                [
-                                    html.Td(id="emotion_3"),
-                                    html.Td(id="emotion_score_3"),
-                                ]
-                            ),
-                        ]
-                    ),
-                ],
-                style={"display": "none"},
-                id="emotion-classification-table",
-            ),
-            html.Br(),
-            html.H6("Explanations:"),
-            dbc.Card(
-                [
-                    dcc.Dropdown(
-                        id="global-explanation-feature-dropdown",
-                        options=["Overall"] + get_emotion_mms(),
-                        multi=False,
-                        value="Overall",
-                    ),
-                    dcc.Graph(
-                        id="global-explanation",
-                        style={"display": "none"},
-                    ),
-                ],
-            ),
-        ]
-    )
-)
-
-empathy_analysis = dbc.Card(
-    [
-        html.H4("Empathy Analysis"),
-        html.Pre(id="empathy"),
-        html.Table(
-           [
-               html.Thead(
-                   html.Tr(
-                       [
-                           html.Th(""),
-                           html.Th("Emotional Reaction"),
-                           html.Th("Interpretation"),
-                           html.Th("Exploration"),
-                       ],
-                   ),
-                   style={"hidden": True},
-               ),
-               html.Tbody(
-                   [
-                        html.Tr(
-                           [
-                               html.Td("Micromodels", style={"font-weight": "bold"}),
-                               html.Td(id="emotional_reaction_mm"),
-                               html.Td(id="interpretation_mm"),
-                               html.Td(id="exploration_mm"),
-                           ]
-                       ),
-                       html.Tr(
-                           [
-                               html.Td("Epitome", style={"font-weight": "bold"}),
-                               html.Td(id="emotional_reaction_epitome"),
-                               html.Td(id="interpretation_epitome"),
-                               html.Td(id="exploration_epitome"),
-                           ]
-                       ),
-                   ]
-               ),
-           ],
-           #style={"display": "none"},
-           id="empathy-table",
+                            dbc.Tab(label="Empathy", tab_id="utterance-tab-3"),
+                        ],
+                        id="utterance-tabs",
+                        active_tab="utterance-tab-1",
+                    )
+                ),
+                dbc.CardBody(
+                    [
+                        html.H5(id="speaker"),
+                        html.Br(),
+                        html.Div(id="utterance"),
+                    ],
+                    id="utterance-card",
+                ),
+            ]
         )
-    ]
+    ],
+    type="circle",
 )
 
-pair_analysis = dbc.Card(
-    [
-        html.H4("MITI Analysis"),
-        html.Pre(id="pair")
-    ]
+
+micromodel_component = dcc.Loading(
+    id="loading-mms",
+    children=[
+        dbc.Card(
+            [
+                dcc.Graph(id="micromodel-results", style={"display": "none"}),
+                dcc.Store(id="emotion-classification-storage"),
+            ]
+        )
+    ],
+    type="circle",
 )
+
+emotion_analysis = dcc.Loading(
+    id="loading-emotions",
+    children=[
+        dbc.Card(
+            dbc.CardBody(
+                [
+                    html.H4("Emotion Analysis"),
+                    html.Table(
+                        [
+                            html.Thead(
+                                html.Tr(
+                                    [
+                                        html.Th("Emotion"),
+                                        html.Th("Confidence Score"),
+                                    ]
+                                ),
+                                style={"hidden": True},
+                            ),
+                            html.Tbody(
+                                [
+                                    html.Tr(
+                                        [
+                                            html.Td(id="emotion_1"),
+                                            html.Td(id="emotion_score_1"),
+                                        ]
+                                    ),
+                                    html.Tr(
+                                        [
+                                            html.Td(id="emotion_2"),
+                                            html.Td(id="emotion_score_2"),
+                                        ]
+                                    ),
+                                    html.Tr(
+                                        [
+                                            html.Td(id="emotion_3"),
+                                            html.Td(id="emotion_score_3"),
+                                        ]
+                                    ),
+                                ]
+                            ),
+                        ],
+                        style={"display": "none"},
+                        id="emotion-classification-table",
+                    ),
+                    html.Br(),
+                    html.H6("Explanations:"),
+                    dbc.Card(
+                        [
+                            dcc.Dropdown(
+                                id="global-explanation-feature-dropdown",
+                                options=["Overall"] + get_emotion_mms(),
+                                multi=False,
+                                value="Overall",
+                            ),
+                            dcc.Graph(
+                                id="global-explanation",
+                                style={"display": "none"},
+                            ),
+                        ],
+                    ),
+                ]
+            )
+        )
+    ],
+    type="circle",
+)
+
+
+empathy_analysis = dcc.Loading(
+    id="loading-empathy",
+    children=[
+        dbc.Card(
+            [
+                html.H4("Empathy Analysis"),
+                html.Pre(id="empathy"),
+                html.Table(
+                    [
+                        html.Thead(
+                            html.Tr(
+                                [
+                                    html.Th(""),
+                                    html.Th("Emotional Reaction"),
+                                    html.Th("Interpretation"),
+                                    html.Th("Exploration"),
+                                ],
+                            ),
+                            style={"hidden": True},
+                        ),
+                        html.Tbody(
+                            [
+                                html.Tr(
+                                    [
+                                        html.Td(
+                                            "Micromodels",
+                                            style={"font-weight": "bold"},
+                                        ),
+                                        html.Td(id="emotional_reaction_mm"),
+                                        html.Td(id="interpretation_mm"),
+                                        html.Td(id="exploration_mm"),
+                                    ]
+                                ),
+                                html.Tr(
+                                    [
+                                        html.Td(
+                                            "Epitome",
+                                            style={"font-weight": "bold"},
+                                        ),
+                                        html.Td(
+                                            id="emotional_reaction_epitome"
+                                        ),
+                                        html.Td(id="interpretation_epitome"),
+                                        html.Td(id="exploration_epitome"),
+                                    ]
+                                ),
+                            ]
+                        ),
+                    ],
+                    # style={"display": "none"},
+                    id="empathy-table",
+                ),
+            ]
+        )
+    ],
+    type="circle",
+)
+
+pair_analysis = dbc.Card([html.H4("MITI Analysis"), html.Pre(id="pair")])
 
 
 analysis = html.Div(
     [
-        utterance_component,
-        html.Br(),
-        micromodel_component,
-        html.Br(),
-        emotion_analysis,
-        empathy_analysis,
-        pair_analysis,
-    ],
-    style={"display": "none"},
-    id="analysis",
+        dcc.Loading(
+            id="loading-analysis",
+            children=[
+                html.Div(
+                    [
+                        utterance_component,
+                        html.Br(),
+                        micromodel_component,
+                        html.Br(),
+                        emotion_analysis,
+                        empathy_analysis,
+                        pair_analysis,
+                    ],
+                    style={"display": "none"},
+                    id="analysis",
+                ),
+            ],
+            type="circle",
+        )
+    ]
 )
